12679116c1433a1623f62ad6f9446dc4c23a82db
loveEthing <838568619@qq.com>
2018-11-21 01:14:05 +0800

    NameThreadFactory

--- src/main/java/gl/common/thread/NameThreadFactory.java
+++ src/main/java/gl/common/thread/NameThreadFactory.java
@@ -1,5 +1,31 @@
 package gl.common.thread;
 
-public class NameThreadFactory {
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
 
+public class NameThreadFactory implements ThreadFactory {
+    private final ThreadGroup group;
+    private final AtomicInteger threadNumber = new AtomicInteger(1);
+
+    private final String namePrefix;
+    private final boolean daemon;
+
+    public NameThreadFactory(String namePrefix, boolean daemon) {
+        this.daemon = daemon;
+        SecurityManager s = System.getSecurityManager();
+        group = (s != null) ? s.getThreadGroup() :
+                Thread.currentThread().getThreadGroup();
+        this.namePrefix = namePrefix;
+    }
+
+    public NameThreadFactory(String namePrefix) {
+        this(namePrefix, false);
+    }
+
+    @Override
+    public Thread newThread(Runnable r) {
+        Thread t = new Thread(group, r, namePrefix + "-thread-" + threadNumber.getAndIncrement(), 0);
+        t.setDaemon(daemon);
+        return t;
+    }
 }
